8b7a75d87e3903ad2f4ba4e16b59d2e645d81bbb
george-ar <gaccap@gmail.com>
2016-05-11 22:07:16 -0600

    commit

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
-angular.module('appTareas', ['ui.router'])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('alta', {
-                url: '/alta',
-                templateUrl: 'views/alta.html',
-                controller: 'ctrlAlta'
-            })
-            .state('editar', {
-                url: '/editar/{id}',
-                templateUrl: 'views/editar.html',
-            });
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-        $urlRouterProvider.otherwise('alta');
-    })
-    .controller('ctrlAlta', function($scope) {
-        $scope.tarea = {}
-        $scope.tareas = [];
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-        $scope.prioridades = ['Baja', 'Normal', 'Alta'];
+var app = express();
 
-        $scope.agregar = function() {
-            $scope.tareas.push({
-                nombre: $scope.tarea.nombre,
-                prioridad: parseInt($scope.tarea.prioridad)
-            })
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-            $scope.tarea.nombre = '';
-            $scope.tarea.prioridad = '';
-        }
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-        $scope.masPrioridad = function(tarea) {
-            tarea.prioridad += 1;
-        }
+app.use('/', routes);
+app.use('/users', users);
 
-        $scope.menosPrioridad = function(tarea) {
-            tarea.prioridad -= 1;
-        }
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-        //eliminar
-        $scope.eliminar = function(tarea){
-            var indice = $scope.tareas.indexOf(tarea);
-            $scope.tareas.splice(indice, 1);
-        }
+// error handlers
 
-    })
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
